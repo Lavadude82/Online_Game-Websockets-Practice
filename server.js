e8a4e01d3d3e89ddc3aef63222218ac89da6a2e1
Lavadude82 <98785801+Lavadude82@users.noreply.github.com>
2023-04-16 07:30:47 -0500

    Bing Added Comments
    I was too lazy :P

--- server.js
+++ server.js
@@ -1,31 +1,40 @@
-// import(s) required
-let crypto = require('crypto');
+// import required modules
+let crypto = require("crypto");
 const express = require("express");
 const fs = require("fs");
-const { measureMemory } = require("vm");
 const app = express();
 const ws = require("ws");
 const server = new ws.Server({ port: 5480 });
 const port = 5500;
 
+// function to generate a random key of a given length
 function generate_key(length) {
-  return crypto.randomBytes(length).toString('base64');
-};
+  return crypto.randomBytes(length).toString("base64");
+}
+
+// function to host files from a given directory
 function HostDir(directory, setHTML, HTMLaltPath) {
+  // read the contents of the directory
   fs.readdirSync(__dirname + directory).forEach((e, i) => {
+    // if the file is not index.html
     if (e != "index.html") {
+      // create a route to serve the file
       app.get(`${directory}${e}`, (req, res) => {
         res.sendfile(__dirname + `${directory}${e}`);
       });
       console.log("hosting " + directory + e);
     } else {
+      // if setHTML is true
       if (setHTML === true) {
+        // if an alternate path for index.html is provided
         if (HTMLaltPath != undefined && HTMLaltPath != null) {
+          // create a route to serve index.html at the alternate path
           app.get(`${HTMLaltPath}`, (req, res) => {
             res.sendfile(__dirname + `${directory}${e}`);
           });
           console.log("hosting " + directory + e + " on " + HTMLaltPath);
         } else {
+          // create a route to serve index.html at the root path
           app.get(`/`, (req, res) => {
             res.sendfile(__dirname + `${directory}${e}`);
           });
@@ -35,14 +44,20 @@ function HostDir(directory, setHTML, HTMLaltPath) {
     }
   });
 }
+
+// template for player data
 const template = {
   username: "",
   pos: { x: 0, y: 0 },
 };
 let players = [];
+
+// handle new connections to the WebSocket server
 server.on("connection", function connection(ws) {
   const p_id = players.length;
   players[p_id] = template;
+  
+  // handle incoming messages from the client
   ws.on("message", function incoming(message) {
     console.log("received: %s", message);
     if (message != "user") {
@@ -54,24 +69,34 @@ server.on("connection", function connection(ws) {
 
     ws.send(JSON.stringify({ id: p_id }));
   });
+  
+  // handle disconnections from the WebSocket server
   server.on("close", function close() {
     console.log(`${players[p_id].username} disconnected`);
     players.splice(p_id, 1);
   });
 });
 
+// host files from various directories
 HostDir("/web-ui/home/", true, "/");
 HostDir("/web-ui/game/", true, "/game");
 HostDir("/web-ui/signin/", true, "/signin");
 HostDir("/web-ui/signup/", true, "/signup");
+
+// route to handle user signup
 app.get("/signup/user", (req, res) => {
+  // get the username and password from the query parameters
   const user = req.query.user;
   const password = req.query.pass;
+
+  // redirect the user to the signin page
   res.send("/signin");
 });
-app.get("/signin/user", (req, res) => {
- 
-});
+
+// route to handle user signin
+app.get("/signin/user", (req, res) => {});
+
+// start the Express server
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
 });
